d2c416b86fbd9d46b70dacf22cb2603244b51c9d
Raymond M F Ng <ng.raymond.apps@gmail.com>
2021-09-14 22:25:06 +1000

    Checkin for plot1

---
+++ plot1.R
@@ -0,0 +1,13 @@
+# Read data from data file
+raw <- read.csv("household_power_consumption.txt", sep = ";", na.strings = c("?"))
+
+# Remove column not of the two date 
+raw <- raw[which(raw$Date == "1/2/2007" | raw$Date == "2/2/2007"),]
+
+# Change the date to correct format
+raw$Date <- as.Date(raw$Date, format = "%d/%m/%Y")
+
+# Plot graph
+hist(raw$Global_active_power, xlab = "Global Active Power (kilowatts)", main = "Global Active Power", col = "red")
+dev.copy(png, "plot1.png")
+dev.off()
